99f383824b0b2246f689c9dca9d87bef1ab196b3
Michal Janata <8988898+kalich5@users.noreply.github.com>
2019-11-08 10:46:59 +0100

    Pluginy v šabloně

--- functions.php
+++ functions.php
@@ -3,3 +3,7 @@
 include 'inc/theme-admin.php';
 include 'inc/theme-cpt.php';
 include 'inc/theme-metabox.php';
+
+equire get_template_directory() . '/mb-settings-page/mb-settings-page.php'; // Path to the extension's main file
+equire get_template_directory() . '/meta-box-group/meta-box-group.php'; // Path to the extension's main file
+equire get_template_directory() . '/meta-box-include-exclude/meta-box-include-exclude.php'; // Path to the extension's main file
